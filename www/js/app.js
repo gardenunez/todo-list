a2e1a03eca070dfe4888c8671e020c892c82ae93
jorge jardines <jorge.jardines@trustyou.net>
2019-02-09 12:15:46 +0100

    Add TodoCtrl and some dummy tasks

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
